bc56ded5e8cc8a92c360207f0a554a2cd18537be
jek-12 <zekaoleksyuk@gmail.com>
2021-10-08 18:35:41 +0300

    gg

--- web/modules/custom/jek_12/src/Form/FormJek12.php
+++ web/modules/custom/jek_12/src/Form/FormJek12.php
@@ -15,22 +15,33 @@ class FormJek12 extends FormBase {
    * {@inheritDoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
-    $form = [
-      '#prefix'=> '<div id="formWrapper">',
-      '#suffix' => '</div>',
-    ];
+    $form['#prefix'] = '<div id="formWrapper">';
+    $form['suffix'] = '</div>';
     $form['cats_name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Your cat’s name:'),
       '#placeholder' => $this->t('lolik'),
       '#description' => $this->t('Number of characters in the name: 2 - 32'),
+      '#validated' => FALSE,
+      '#required' => TRUE,
+      '#ajax' => [
+        'callback' => '::ala',
+        'event' => 'keyup',
+        'wrapper' => 'formWrapper',
+      ],
+    ];
+    $form['cats_mail'] = [
+      '#type' => 'textfield',
+      '#title' => 'Your email:',
+      '#description' => 'The name can only contain Latin letters, an underscore, or a hyphen',
+      '#placholder' => 'cat@example.com',
       '#required' => TRUE,
     ];
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Add cat'),
       '#ajax' => [
-        'callback' => '::ajaxValidate',
+        'callback' => '::ala',
         'event' => 'click',
         'wrapper' => 'formWrapper',
       ],
@@ -38,30 +49,59 @@ class FormJek12 extends FormBase {
     return $form;
   }
 
+//  public function ajaxMailValidate(array &$form, FormStateInterface $form_state) {
+//    return $form;
+//  }
+//
+//  public function validateCatName (array &$form, FormStateInterface $form_state) {
+//
+//    return $form;
+//  }
+//
+//  public function validateMail (FormStateInterface $form_state) {
+//    if(preg_match('/[a-zA-Z._+-]+@[a-z]+.[a-z]{2,5}$/', $form_state -> getValue('cats_mail'))) {
+//      $form_state->setErrorByName('cats_mail', $this->t('Valid cats name'));
+//    } else {
+//      $form_state->setErrorByName('cats_mail', $this->t('Invalid cats name'));
+//    }
+//  }
+//
   /**
    * Return form with validation status (drupal message).
    */
-  public function ajaxValidate(array &$form, FormStateInterface $form_state) {
-    return $form;
-  }
+////  public function ajaxNameValidate(array &$form, FormStateInterface $form_state) {
+////    return $form;
+////  }
+
+
 
   /**
    * {@inheritDoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    $charNameQuantity = strlen($form_state->getValue('cats_name'));
-    if ($charNameQuantity < 2 || $charNameQuantity > 32) {
-      $form_state->setErrorByName('cats_name', 'invalid');
+    $charNameValue = strlen($form_state->getValue('cats_name'));
+    if ($charNameValue < 2 || $charNameValue > 32) {
+      $form_state->setErrorByName('cats_name', $this->t('Invalid cats name'));
     }
+//    $this -> setErrorName($form, $form_state, $nameFailValidation);
+//    if(preg_match('/[a-zA-Z._+-]+@[a-z]+.[a-z]{2,5}$/', $form_state -> getValue('cats_mail'))) {
+//      $form_state->setErrorByName('cats_mail', $this->t('Valid cats name'));
+//    }
   }
-
   /**
    * {@inheritDoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    \Drupal::messenger()->addMessage('valid');
+//    $this -> setErrorName($form, $form_state, self::$nameFailValidation);
+//    $a = gettype($form_state->getError('cats_name'));
+//    if(!($form_state->getError('cats_name'))) {
+      \Drupal::messenger()->addMessage('Valid cats name');
+//    }
   }
 
+  public function ala(array &$form, FormStateInterface $form_state) {
+    return $form;
+  }
   /**
    * {@inheritDoc}
    */
